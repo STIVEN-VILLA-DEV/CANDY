ee233b4db95dece51b6afdcd2839ea015870a473
Stiven Villa <stivenvilla2001@gmail.com>
2026-03-31 10:22:31 -0500

    deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider appearance={clerkTheme}
+    <ClerkProvider  publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+       appearance={clerkTheme}
       proxyUrl={undefined} 
       domain={undefined}
     >
